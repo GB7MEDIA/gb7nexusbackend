a154fd4931ee9227ae06d4190104c934e453ff98
Grispin Bauknecht <grispinbauknecht@Grispins-MacBook-Pro-2.local>
2024-03-18 10:16:44 +0100

    chnages

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
